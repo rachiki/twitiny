96c8c4d25ca55da15cf66bbdf013f46c331ab34b
rachiki <ralle.kinkel@gmail.com>
2023-11-23 13:00:42 +0100

    Finished registration, not checking format yet

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, Request, Form, status
+from fastapi import FastAPI, Depends, Request, Form, status, HTTPException
 
 from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
@@ -23,12 +23,34 @@ def get_db():
 
 @app.get("/")
 def index(request: Request, db: Session = Depends(get_db)): # auto error gets thrown if connection fails
-    login = db.query(data_models.User).first()
     return templates.TemplateResponse("login.html", 
-                                      {"request": request, "login": login})
+                                      {"request": request})
 
-@app.get("/")
-def index(request: Request, db: Session = Depends(get_db)): # auto error gets thrown if connection fails
-    login = db.query(data_models.User).first()
-    return templates.TemplateResponse("login.html", 
-                                      {"request": request, "login": login})
+@app.get("/register")
+def index(request: Request, db: Session = Depends(get_db)): 
+    return templates.TemplateResponse("register.html", 
+                                      {"request": request})
+
+@app.post("/create_user")
+def create_user(username: str = Form(...), password: str = Form(...), email: str = Form(...),
+                 db: Session = Depends(get_db)):
+    new_user = data_models.User(username=username, password=password, email=email)
+
+    # Check if the username already exists
+    existing_user = db.query(data_models.User).filter(data_models.User.username == new_user.username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already taken")
+    
+    # Check if the email already exists
+    existing_mail = db.query(data_models.User).filter(data_models.User.email == new_user.email).first()
+    if existing_mail:
+        raise HTTPException(status_code=400, detail="E-Mail already registered")
+
+    db.add(new_user)
+    db.commit()
+    return RedirectResponse(url="/register_success", status_code=303)
+
+@app.get("/register_success")
+def register_success(request: Request, db: Session = Depends(get_db)): 
+    return templates.TemplateResponse("register_success.html", 
+                                      {"request": request})
